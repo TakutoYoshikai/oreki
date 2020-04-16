f95e3b2e5f11e1049c4e2cd891727dec7c6d0a62
EC2 Default User <ec2-user@ip-172-31-38-166.ap-northeast-1.compute.internal>
2020-04-16 06:47:58 +0000

    add example

---
+++ example/main.js
@@ -0,0 +1,36 @@
+const Oreki = require("oreki-node").Oreki
+const oreki = new Oreki("config.json");
+const express = require("express")
+const app = express()
+
+let point = 0
+app.get("/", function(req, res, next) {
+  if (point === 0) {
+    res.json({ msg: "failed" })
+  } else {
+    res.json({ msg: "success"})
+    point -= 1
+  }
+})
+
+app.listen(3000, function() {
+  console.log("started")
+});
+
+(async() => {
+  const initialized = await oreki.init()
+  if (!initialized) {
+    console.error("couldn't initialized")
+    return
+  }
+  oreki.on("paid", function(payment) {
+    console.log("PAID")
+    console.log(payment)
+    point += payment.point
+  })
+  oreki.start()
+  let payment = await oreki.addPayment("user", "endpoint", 5, 1000)
+  console.log("address: ", payment.address)
+})()
+
+
