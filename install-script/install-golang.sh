3f2a91374f5e5beae3e8334d9e778edb7320660a
takuto <takuto.yoshikai@gmail.com>
2020-05-13 15:33:09 +0900

    gopath, goroot

--- install-script/install-golang.sh
+++ install-script/install-golang.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 
+
 cd /tmp
 wget https://storage.googleapis.com/golang/go1.13.linux-amd64.tar.gz
 tar vzfx go1.13.linux-amd64.tar.gz
 sudo mv go /usr/local/
 rm go1.13.linux-amd64.tar.gz
+cd ~
+mkdir go
+
+echo "export GOPATH=$HOME/go"
+echo "export PATH=\$PATH:/usr/local/go/bin:$GOPATH/bin"
+echo "export GOROOT=/usr/local/go"
+read -p "I register those GOROOT and GOPATH and golang binary directory on your .bash_profile. ok? (y/N): " yn
+case "$yn" in [yY]*) ;; *) echo "finished." ; exit ;; esac
+echo "export GOPATH=$HOME/go" >> $HOME/.bash_profile
+echo "export PATH=\$PATH:/usr/local/go/bin:$GOPATH/bin" >> $HOME/.bash_profile
+echo "export GOROOT=/usr/local/go" >> $HOME/.bash_profile
+
